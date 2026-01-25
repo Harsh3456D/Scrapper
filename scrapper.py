ad5e66786be45fd316ddaf9c3e634e48b75c7e43
Harsh Dabas <harshdabas1314@gmail.com>
2026-01-26 04:51:52 +0530

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,22 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scrapper:
+    def __init__(self,site):
+        self.site = site
+        
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html,parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            if url is None:
+                continue
+            if "read" in url:
+                print(f"\n + {url}")
+
+
+news = "https://news.google.com/"
+Scrapper(news).scrape()
